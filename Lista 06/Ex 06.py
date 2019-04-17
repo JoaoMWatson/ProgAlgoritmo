fca6dff3c3691f3546b2ffd975b862d1fe447fd7
Joao Pedro <joaopedrowatson@hotmail..com>
2019-04-16 21:46:24 -0300

    Atualizado

--- Lista 06/Ex 06.py
+++ Lista 06/Ex 06.py
@@ -1,6 +1,6 @@
-x = int(input('Digite um numero menor que 10(Dez): '))
+x = 11
 
 while x >= 10:
-    x = int(input('Numero maior ou igual a dez, tente novamente: '))
+    x = int(input('Digite um numero menor que 9(nove): '))
 
 print(x)
